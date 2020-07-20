6c29909c35697ae54a151728647b908a46ad84eb
jakemandel <63460704+jakemandel@users.noreply.github.com>
2020-07-20 11:00:21 -0700

    Write/read Memory on x and y axis

--- swellpy/read_mem.py
+++ swellpy/read_mem.py
@@ -17,19 +17,18 @@ seed = 125
 m = Monodisperse2(N,Bx,By,seed)
 
 
-area_frac_x = 0.5
-area_frac_y = 0.5
+area_frac = 0.5
 kick = .05
 #swell = m.equiv_swell(area_frac)
 cycle_number = 1000 
-xform = .97
+xform = .95
 
 #m.particle_plot(area_frac, show=True, extend = True, figsize = (7,7), filename=None)
 
 
-count = m.train_xform(xform, 1, area_frac_x, kick, cycle_number, noise=0)
+count = m.train_xform(xform, 1, area_frac, kick, cycle_number, noise=0)
 print(count)
-m.particle_plot(area_frac_x, show=True, extend = True, figsize = (7,7), filename=None)
+m.particle_plot(area_frac, show=True, extend = True, figsize = (7,7), filename=None)
 
 
 area_frac_array = np.array(np.linspace(0,1,150))
@@ -102,79 +101,6 @@ plt.xlabel('Area Fraction')
 plt.legend(['Isotropic', 'X-axis', 'Y-axis'])
 plt.show()
 
-# Transform y axis
-count = m.train_xform(1, xform, area_frac_y, kick, cycle_number, noise=0)
-print(count)
-m.particle_plot(area_frac_y, show=True, extend = True, figsize = (7,7), filename=None)
-area_frac_array = np.array(np.linspace(0,1,150))
-
-func_count_iso = m.tag_count_xform
-func_rate_iso = m.tag_rate_xform
-func_curve_iso = m.tag_curve_xform
-data_count_iso = func_count_iso(area_frac_array, 1, 1)
-data_rate_iso = func_rate_iso(area_frac_array, 1, 1)
-data_curve_iso = func_curve_iso(area_frac_array, 1, 1)
-
-mem1 = m.detect_memory_xform(0, 1, .01, 1,1)
-print('Isotropic:',mem1)
-
-scale_x = xform
-scale_y = 1
-for i in m.centers: #Transform centers along readout axis
-    i[0] = i[0]*(scale_x/scale_y)
-    i[1] = i[1]*(scale_y/scale_x)
-func_count_x = m.tag_count_xform 
-func_rate_x = m.tag_rate_xform
-func_curve_x = m.tag_curve_xform
-data_count_x = func_count_x(area_frac_array, scale_x, scale_y)
-data_rate_x = func_rate_x(area_frac_array, scale_x, scale_y)
-data_curve_x = func_curve_x(area_frac_array, scale_x, scale_y)
-for i in m.centers: #Transform centers back
-    i[0] = i[0]*(scale_y/scale_x)
-    i[1] = i[1]*(scale_x/scale_y)
-    
-mem2 = m.detect_memory_xform(0, 1, .01, scale_x,scale_y)
-print('x-axis:', mem2)
-    
-scale_x = 1
-scale_y = xform
-for i in m.centers: #Transform centers along readout axis
-    i[0] = i[0]*(scale_x/scale_y)
-    i[1] = i[1]*(scale_y/scale_x)    
-func_count_y = m.tag_count_xform
-func_rate_y = m.tag_rate_xform
-func_curve_y = m.tag_curve_xform
-data_count_y = func_count_y(area_frac_array, scale_x, scale_y)
-data_rate_y = func_rate_y(area_frac_array, scale_x, scale_y)
-data_curve_y = func_curve_y(area_frac_array, scale_x, scale_y)
-for i in m.centers: #Transform centers back
-    i[0] = i[0]*(scale_y/scale_x)
-    i[1] = i[1]*(scale_x/scale_y)
-
-mem3 = m.detect_memory_xform(0, 1, .01, scale_x,scale_y)
-print('y-axis:',mem3)
-
-plt.plot(area_frac_array, data_count_iso)
-plt.plot(area_frac_array, data_count_x)
-plt.plot(area_frac_array, data_count_y)
-plt.ylabel('Count')
-plt.xlabel('Area Fraction')
-plt.legend(['Isotropic', 'X-axis', 'Y-axis'])
-plt.show()
-plt.plot(area_frac_array, data_rate_iso)
-plt.plot(area_frac_array, data_rate_x)
-plt.plot(area_frac_array, data_rate_y)
-plt.ylabel('Rate')
-plt.xlabel('Area Fraction')
-plt.legend(['Isotropic', 'X-axis', 'Y-axis'])
-plt.show()
-plt.plot(area_frac_array, data_curve_iso)
-plt.plot(area_frac_array, data_curve_x)
-plt.plot(area_frac_array, data_curve_y)
-plt.ylabel('Curve')
-plt.xlabel('Area Fraction')
-plt.legend(['Isotropic', 'X-axis', 'Y-axis'])
-plt.show()
 
 #%%
 
--- swellpy/ssparameters.py
+++ swellpy/ssparameters.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 
 
-area_fraction = [.5,.51,.52,.53,.54,.55,.56,.57,.58,.59,.6]
+area_fraction = [.8,.81,.82,.83,.84,.85,.86,.87,.88,.89,.9]
 for area_frac in area_fraction:
     N = 1000 
     Bx = 40 #box length (x)
@@ -22,7 +22,7 @@ for area_frac in area_fraction:
     kick = .05
     swell = m.equiv_swell(area_frac)
     cycle_number = 10000 
-    xform = .2
+    xform = .9
     
     count = m.train_xform(xform, 1, area_frac, kick, cycle_number, noise=0)
     #m.particle_plot(area_frac, show=True, extend = True, figsize = (7,7), filename=None)
@@ -43,38 +43,75 @@ for area_frac in area_fraction:
 from monodisperse_box_xform import Monodisperse2
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 #transform on x axis:
     #Points that reached steady state:
 
-xform1 = [.1,.1,.1,.1,.1] 
-xform2 = [.2,.2,.2,.2,.2]
-xform3 = [.3,.3,.3,.3,.3] 
-xform4 = [.4,.4,.4,.4]
-xform5 = [.5,.5,.5,.5]
-xform6 = [.6,.6,.6,.6,.6,.6]
-xform7 = [.7,.7,.7,.7,.7,.7]
-xform8 = [.8,.8,.8,.8,.8,.8,.8]
-xform9 = [.9,.9,.9,.9,.9,.9,.9,.9]
+xform1 = [.1,.1,.1,.1,.1,.1] 
+xform2 = [.2,.2,.2,.2,.2,.2,.2,.2,.2]
+xform3 = [.3,.3,.3,.3,.3,.3,.3,.3,.3] 
+xform4 = [.4,.4,.4,.4,.4,.4,.4,.4,.4,.4]
+xform5 = [.5,.5,.5,.5,.5,.5,.5,.5,.5,.5,.5]
+xform6 = [.6,.6,.6,.6,.6,.6,.6,.6]
+xform7 = [.7,.7,.7,.7,.7,.7,.7,.7,.7,.7,.7,.7,.7]
+xform8 = [.8,.8,.8,.8,.8,.8,.8,.8,.8,.8,.8,.8]
+xform9 = [.9,.9,.9,.9,.9,.9,.9,.9,.9,.9,.9,.9,.9]
   
-af_x1 = [.1,.2,.3,.4,.5]
-af_x2 = [.1,.2,.3,.4,.5]
-af_x3 = [.1,.2,.3,.4,.5]
-af_x4 = [.1,.2,.3,.4]
-af_x5 = [.1,.2,.3,.4]
-af_x6 = [.1,.2,.3,.4,.5,.6]
-af_x7 = [.1,.2,.3,.4,.5,.6]
-af_x8 = [.1,.2,.3,.4,.5,.6,.7]
-af_x9 = [.1,.2,.3,.4,.5,.6,.7,.8]
+af_1 = [.1,.2,.3,.4,.5,.52]
+af_2 = [.1,.2,.3,.4,.5,.51,.53,.54,.55]
+af_3 = [.1,.2,.3,.4,.5,.51,.52,.53,.54]
+af_4 = [.1,.2,.3,.4,.42,.45,.46,.47,.48,.49]
+af_5 = [.1,.2,.3,.4,.42,.45,.46,.47,.48,.49,.53]
+af_6 = [.1,.2,.3,.4,.5,.6,.61,.62]
+af_7 = [.1,.2,.3,.4,.5,.6,.61,.62,.63,.64,.65,.66,.67]
+af_8 = [.1,.2,.3,.4,.5,.6,.7,.71,.72,.73,.74,.75]
+af_9 = [.1,.2,.3,.4,.5,.6,.7,.8,.81,.82,.83,.84,.85]
 
-plt.scatter(xform1,af_x1,marker="*")
-plt.scatter(xform2,af_x2,marker="*")
-plt.scatter(xform3,af_x3,marker="*")
-plt.scatter(xform4,af_x4,marker="*")
-plt.scatter(xform5,af_x5,marker="*")
-plt.scatter(xform6,af_x6,marker="*")
-plt.scatter(xform7,af_x7,marker="*")
-plt.scatter(xform8,af_x8,marker="*")
-plt.scatter(xform9,af_x9,marker="*")
+xform = xform1+xform2+xform3+xform4+xform5+xform6+xform7+xform8+xform9
+af = af_1 + af_2 + af_3 + af_4 + af_5 + af_6 + af_7 + af_8 + af_9
+
+# Points that do not reach steady state
+xform1x = [.1,.1,.1]
+xform2x = [.2,.2]
+xform3x = [.3,.3]
+xform4x = [.4,.4,.4]
+xform5x = [.5,.5,.5,.5,.5]
+xform6x = [.6,.6,.6]
+xform7x = [.7,.7,.7]
+xform8x = [.8,.8,.8]
+xform9x = [.9,.9]
+
+af_1x = [.51,.53,.54]
+af_2x = [.52,.56]
+af_3x = [.55,.56]
+af_4x = [.5,.51,.52]
+af_5x = [.5,.51,.52,.54,.55]
+af_6x = [.63,.64,.65]
+af_7x = [.68,.69,.7]
+af_8x = [.76,.77,.78]
+af_9x = [.86,.87]
+
+xformx = xform1x+xform2x+xform3x+xform4x+xform5x+xform6x+xform7x+xform8x+xform9x
+afx = af_1x+af_2x+af_3x+af_4x+af_5x+af_6x+af_7x+af_8x+af_9x
+
+boundary_x = [.1,.2,.3,.4,.5,.6,.7,.8,.9]
+boundary_y = [.52,.55,.54,.49,.49,.62,.67,.75,.85]
+plt.figure(figsize=(10,7), dpi= 80)
+sns.set_style("darkgrid")
+plt.plot(boundary_x,boundary_y,"g")
+# plt.scatter(xform1,af_1,marker="*")
+# plt.scatter(xform2,af_2,marker="*")
+# plt.scatter(xform3,af_3,marker="*")
+# plt.scatter(xform4,af_4,marker="*")
+# plt.scatter(xform5,af_5,marker="*")
+# plt.scatter(xform6,af_6,marker="*")
+# plt.scatter(xform7,af_7,marker="*")
+# plt.scatter(xform8,af_8,marker="*")
+# plt.scatter(xform9,af_9,marker="*")
+plt.scatter(xform,af,color="g",marker="*")
+plt.scatter(xformx,afx,color="r",marker="x")
+
 plt.ylabel('Area Fraction')
-plt.xlabel('Transform')
+plt.xlabel('Transform (scaling factor)')
 plt.show()
+
